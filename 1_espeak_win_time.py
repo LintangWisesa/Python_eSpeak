59340c7072ce71952b6d875b0eb2068333f6c370
lintangwisesa <lintangwisesa@ymail.com>
2018-11-23 17:11:41 +0700

    python espeak

--- 1_espeak_win_time.py
+++ 1_espeak_win_time.py
@@ -1,11 +1,16 @@
 import subprocess
 import datetime
-
 now = datetime.datetime.now()
+
 tahun = now.year
-bulan = now.strftime("%B")
-tanggal = now.strftime("%d")
-hari = now.strftime("%A")
+bulan = now.strftime("%B")      #  %b  %m
+tanggal = now.strftime("%d")    
+hari = now.strftime("%A")       #  %a  %w
+jam = now.strftime("%H")        #  %I
+menit = now.strftime("%M")
+detik = now.strftime("%S") 
+
+waktulokal = now.strftime("%c")      #  %x  %X
 
 path = 'C:\Program Files (x86)\eSpeak\command_line\espeak.exe'
 text = str('Now is ' + hari + ', ' + str(tanggal) + ' ' + bulan + ' ' + str(tahun))
